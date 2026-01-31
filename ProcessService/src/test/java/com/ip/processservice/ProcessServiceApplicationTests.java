de9e8037b0254cc566fa4912b9e5e76393759391
tungluuquang <tungq2005@gmail.com>
2026-01-31 17:50:37 +0700

    [Feat] The definition of several services.

---
+++ ProcessService/src/test/java/com/ip/processservice/ProcessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ip.processservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
